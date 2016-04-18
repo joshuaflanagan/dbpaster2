9ed4dacc0a617862c879b2c036d985a7bffcc488
Joshua Flanagan <joshuaflanagan@gmail.com>
2016-04-17 20:51:23 -0500

    Add support for Redux DevTools Chrome extension
    https://github.com/zalmoxisus/redux-devtools-extension

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import createLogger from 'redux-logger';
 import rootReducer from 'src/reducers'
 
@@ -10,7 +10,10 @@ export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(logger)
+    compose(
+      applyMiddleware(logger),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
   )
 
   if (module.hot) {
